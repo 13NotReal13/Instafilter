eac30dbdee58534e5122cb21440e8f6003a067be
Ivan Semikin <i.semikin.94@icloud.com>
2024-11-06 18:31:06 +0100

    Completed tasks

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,52 +6,131 @@
 //
 
 import SwiftUI
+import PhotosUI
 import CoreImage
 import CoreImage.CIFilterBuiltins
+import StoreKit
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var amount: Float = 10
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    @State private var filterRadius = 0.5
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var showingFilters = false
+    
+    @AppStorage("filterCount") var filterCount = 0
+    @Environment(\.requestReview) var requestReview
+    
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Slider(value: $amount, in: 0...1000)
-        }
-        .onAppear(perform: loadImage)
-        .onChange(of: amount) { oldValue, newValue in
-            loadImage()
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
+                }
+                .onChange(of: selectedItem, loadImage)
+                
+                Spacer ()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity, in: 0...1) { _ in
+                        applyProcessing()
+                    }
+                    .disabled(selectedItem == nil)
+                }
+
+                HStack {
+                    Text("Radius")
+                    Slider(value: $filterRadius, in: 0...1) { _ in
+                        applyProcessing()
+                    }
+                    .disabled(selectedItem == nil)
+                }
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                        .disabled(selectedItem == nil)
+                    
+                    Spacer()
+                    
+                    if let processedImage {
+                        ShareLink(item: processedImage, preview: SharePreview("Instafilter image", image: processedImage))
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+            .confirmationDialog("Select a filter", isPresented: $showingFilters) {
+                Button("Crystallize") { setFilter(CIFilter.crystallize() )}
+                Button("Edges") { setFilter(CIFilter.edges() )}
+                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur() )}
+                Button("Pixellate") { setFilter(CIFilter.pixellate() )}
+                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone() )}
+                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask() )}
+                Button("Vignette") { setFilter(CIFilter.vignette() )}
+                Button("Motion Blur") { setFilter(CIFilter.motionBlur() )}
+                Button("Bloom") { setFilter(CIFilter.bloom()) }
+                Button("Comic Effect") { setFilter(CIFilter.comicEffect()) }
+                Button("Cancel", role: .cancel) { }
+            }
         }
     }
     
-    private func loadImage() {
-        let inputImage = UIImage(resource: .icon)
-        let beginImage = CIImage(image: inputImage)
+    @MainActor func changeFilter() {
+        showingFilters = true
         
-        let context = CIContext()
-        let currentFilter = CIFilter.twirlDistortion()
+        filterCount += 1
         
-        currentFilter.inputImage = beginImage
+        if filterCount >= 20 {
+            requestReview()
+        }
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
+        }
+    }
+    
+    func applyProcessing() {
         let inputKeys = currentFilter.inputKeys
         
         if inputKeys.contains(kCIInputIntensityKey) {
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+            currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
         }
         if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(amount, forKey: kCIInputRadiusKey)
+            currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)
         }
         if inputKeys.contains(kCIInputScaleKey) {
-            currentFilter.setValue(amount, forKey: kCIInputScaleKey)
+            currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)
         }
         
         guard let outputImage = currentFilter.outputImage else { return }
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
         
         let uiImage = UIImage(cgImage: cgImage)
-        image = Image(uiImage: uiImage)
+        processedImage = Image(uiImage: uiImage)
+    }
+    
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
     }
 }
 
